95f229299314f0b0ee081bb564cb526f3ec762f7
vasolii <116520054+vasolii@users.noreply.github.com>
2024-01-13 11:42:31 +0200

    dokimes allaggghs ths 2

--- Group18Exe2Fun1.m
+++ Group18Exe2Fun1.m
@@ -1,10 +1,14 @@
 function sd_rate=Group18Exe2Fun1(M,bikes1,bikes2)
+warning off all;
     for i=1:M
         vectors1=randi([1,length(bikes1)],100,1);
         vectors2=randi([1,length(bikes2)],100,1);
+        %vectors=randi([1,length(bikes2)],100,1);
         bikes1=bikes1(vectors1);
         bikes2=bikes2(vectors2);
-        h(i)=chi2gof(bikes1,"Expected",bikes2);
+        [bikes1_hist,edges]=histcounts(bikes1);
+        bikes2_hist=histcounts(bikes2,edges);
+        h(i)=chi2gof(bikes1_hist,"Expected",bikes2_hist);
     end
     sum_h=sum(h);%h=1 for rejection
     sd_rate=(M-sum_h)*100/M;
--- Group18Exe2Prog1.m
+++ Group18Exe2Prog1.m
@@ -21,7 +21,35 @@ while j<height(data)
    
 end 
 
-%to have access in a column of a cell use : 
+
+%NOT SURE JUST A TRY 
+%o feboyarios exei 28 meres. Ara gia na exoyn oloi oi antistoixoi mhnes  to idio tha prepei na
+%sbhsw tis hmeromhnies 29,30, 31 (opoy yparxei) toy mhna may ayg kai nov
+dateObjects = datetime(data.Date, 'InputFormat', 'dd-mmm-yyyy'); %format to matlab gia na exw metaprosbash sto date ksexwrista
+
+%ayto isxyei gia olous tous mhnes
+ % for j=1:31
+ %     %gia mhnes 12 3 6 9
+ %     if sum((day(dateObjects)==j & ismember(month(dateObjects),[12 3 6 9])))~=4*24
+ %         todelete=day(dateObjects)==j & ismember(month(dateObjects),[12 3 6 9]);
+ %         data(todelete,:)=[];
+ %         dateObjects(todelete)=[];
+ %     end    
+ %     %gia mhnes 1 4 7 10    
+ %     if sum((day(dateObjects)==j & ismember(month(dateObjects),[1 4 7 10])))~=4*24
+ %         todelete=day(dateObjects)==j & ismember(month(dateObjects),[1 4 7 10]);
+ %         data(todelete,:)=[];
+ %         dateObjects(todelete)=[]; 
+ %     end    
+ %     %gia mhnes 2 5 8 11    
+ %     if sum((day(dateObjects)==j & ismember(month(dateObjects),[2 5 8 11])))~=4*24
+ %         todelete=day(dateObjects)==j & ismember(month(dateObjects),[2 5 8 11]);
+ %         data(todelete,:)=[];
+ %         dateObjects(todelete)=[];     
+ %     end   
+ %  end 
+
+% to have access in a column of a cell use : 
 % name_of_cell(:,"name_of_column")
 winter_bikes=table2array(data(data.Seasons==1,"Rented Bike Count"));
 spring_bikes=table2array(data(data.Seasons==2,"Rented Bike Count"));
@@ -40,5 +68,17 @@ result23=Group18Exe2Fun1(100,spring_bikes,summer_bikes);
 result24=Group18Exe2Fun1(100,spring_bikes,autumn_bikes);
 result34=Group18Exe2Fun1(100,summer_bikes,autumn_bikes);
 
-%SXOLIA GIA TA APOTELESMATA
-
+%SXOLIA GIA TIS PITHANES ALLAGES
+%Nomizw oti oso pio megalo to M toso pio accurate to apotelesma. Me thn
+%allagh omws apo ta 100 sta 1000 blepw mia terastia diafora posostoy kai oso 
+%aykasw to M teinei na plhsiasei to 100% ARA TI FASH????
+%Sxetika me ta deigmata: dokimasa me kai xwris thn afairesh twn antistoixwn
+%hmerwmhniwn poy leipoyn profanws to deigma einai toso megalo (apo to opoio
+%epilegontai oi times) poy den ephreazei toso h yparksh toys h oxi
+%(odhgoymaste se idia pososta)
+% Pragmatika den odhgoyme se kapoio symperasma apo ta parapanw...POY EINAI
+% TO LATHOS OEO? TOYLAXISTON BEBAIA TWRA DEN BGAZEI MONIMWS MHDENIKA!
+%kai sxetika me ta vectors, emena moy bgazei logikh na pairneis apo to idio
+%shmeio antistoixo ths katanomhs gia thn kathe peripwtsh alliws pws mporeis
+%na ta sygkrineis? alla nomizw kai tyxaia na baleis gia to kathena apo thn
+%stigmh poy xrhsimopoioyme pleon to histcounts den xreiazetai aparaithta
